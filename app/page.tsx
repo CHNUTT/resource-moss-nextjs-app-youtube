0ffacc945e7c8058b7b3c548261ac624554da7b3
CHNUTT <chokwittaya.nutt@gmail.com>
2023-09-25 21:37:09 -0500

    [6] - Show error on using component in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import ProductCard from './components/ProductCard';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         it will not reload all JS files, just the page contents
       */}
       <Link href='/users'>Users</Link>
+      <ProductCard/>
     </main>
   );
 }
