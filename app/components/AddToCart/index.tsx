480e015566f6ffd9779b834f294d705f9551c6cb
CHNUTT <chokwittaya.nutt@gmail.com>
2023-09-25 23:38:06 -0500

    [15] - daisyUI set theme

--- app/components/AddToCart/index.tsx
+++ app/components/AddToCart/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log('add')}>Add</button>
+      <button className='btn btn-primary' onClick={() => console.log('add')}>Add</button>
     </div>
   );
 };
