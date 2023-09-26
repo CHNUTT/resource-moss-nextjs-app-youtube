e85c3f3663a6242f407c3efaa4fd32b003b27938
CHNUTT <chokwittaya.nutt@gmail.com>
2023-09-25 21:28:05 -0500

    [4] - using NextJs <Link>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Hello, World!</h1></main>
+    <main>
+      <h1>Hello, World!</h1>
+      {/* 
+        using Link because
+        it is an optimized way provided by Next
+        it will not reload all JS files, just the page contents
+      */}
+      <Link href="/users">Users</Link>
+    </main>
   )
 }
