b6dec3497d0bbf0a8b8594975cfe79df9557f715
CHNUTT <chokwittaya.nutt@gmail.com>
2023-09-25 23:12:15 -0500

    [11] - static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -9,12 +9,23 @@ const UsersPage = async () => {
   // second arguments can of fetch can be (only fetch)
   // { cache: 'no-store' }
   // { next: { revalidate: 10 }}
-  const response = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
+  const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+    cache: 'no-store',
+  });
   const users: User[] = await response.json();
 
   return (
     <>
       <h1>Users</h1>
+      {/* 
+        with no second argument passing into fetch()
+        Next treat this page as a static page
+        this timestamp will not be changed on the production
+
+        on the other hand with { cache: 'no-store' }
+        the timestamp will be changed when the page is refreshed
+      */}
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
